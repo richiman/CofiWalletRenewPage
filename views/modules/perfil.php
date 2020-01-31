06059f049a5f1a65c536e63c258607ca95945cc4
Ricardo Tirado <richiman117@gmail.com>
2020-01-31 11:54:05 -0600

    se actualizo la View de perfil

--- views/modules/perfil.php
+++ views/modules/perfil.php
@@ -1,9 +1,53 @@
 <app-root>
+  <div class="container">
+    <div class="row d-flex justify-content-center text-center">
+      <div class="col-sm-4 shadow  bg-white rounded">
+        <br>
+        <div class="shadow  bg-white rounded "  >
+          <br>
+          <img src="../img/camera.png" width="80" height="80"alt="" >
+          <br> <br>
+        </div>
+        <form class="" action="index.html" method="post">
+          <br>
+           <input type="text" class="form-control shadow text-center  bg-white rounded " id="idnombre" aria-describedby="nombre" placeholder="Nombre completo">
+           <br>
+            <input  type="text" class="form-control shadow text-center bg-white rounded " id="idFecha" aria-describedby="fecha" placeholder="Fecha de nacimiento">
+          <br>  <input type="text" class="form-control shadow text-center bg-white rounded " id="idFecha" aria-describedby="fecha" placeholder="Domicilio">
+          <br>  <input type="text" class="form-control shadow text-center bg-white rounded " id="idFecha" aria-describedby="fecha" placeholder="Telefono">
+          <br>  <input type="text" class="form-control shadow text-center bg-white rounded " id="idFecha" aria-describedby="fecha" placeholder="Correo">
+          <br>  <input type="text" class="form-control shadow text-center bg-white rounded " id="idFecha" aria-describedby="fecha" placeholder="RFC">
+          <br><button class="btn btn-info" type="button" name="button">Guardar</button>
+        </form>
+          <br>
+      </div>
+      <div class="col-sm-1  "><!-- IGNORE --></div>
+      <div class="col-md-6 shadow  bg-white rounded ">
+        <br>  <br>  <br>
+        <h3 class="text-danger">Cuenta verificada o no X</h3>
+        <br>
+        
+        <div class="shadow  bg-white rounded">
+          <br>
+          <h4 class="text-info">Configuracion de contraseña</h4>
+          <p class="" style=" padding-left: 70px; padding-right: 70px;"> <small>Se recomienda usar una contraseña segura y no compartirla con nadie.</small></p>
+          <button class="btn btn-info" type="button" name="button">Configurar</button>
+          <br>
+          <br>
+        </div>
+        <br>
+        <div class="shadow  bg-white rounded">
+          <br>
+          <h4 class="text-info">Configuracion de pin</h4>
+          <p class="" style=" padding-left: 70px; padding-right: 70px;"><small>Se recomienda usar un segur y no compartirla con nadie.</small></p>
+          <button class="btn btn-info" type="button" name="button">Configurar</button>
+          <br>
+          <br>
+        </div>
 
-<h1>Perfil</h1>
-
-
-
+      </div>
+    </div>
+  </div>
 
 
 </app-root>
