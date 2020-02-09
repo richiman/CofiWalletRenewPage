587de347e4cd1e6fd88d0034ebcb64716a0fb873
Ricardo Tirado <richiman117@gmail.com>
2020-02-09 11:39:09 -0600

    Pagos base falta el menu lateral derecho

--- views/modules/pagos.php
+++ views/modules/pagos.php
@@ -1,9 +1,135 @@
 <app-root>
+  <style media="screen">
+  .scrollable {
+    height: 400px; /* or any value */
+    overflow-y: auto;
 
-<h1>Pagos</h1>
+  }
+  .scrollable2 {
+    height: 485px; /* or any value */
+    overflow-y: auto;
 
+  }
+  </style>
+  <div class="container d-flex-center">
+    <div class="row">
+      <div class="col-md-7 shadow bg-white rounded">
+  <div class="scrollable2">
+        <table class="table   ">
 
+ <thead >
+   <tr class=" ">
+     <th class="" scope="col"> <small><strong>Fecha inicial</strong></small> </th>
+     <th scope="col"><small><strong>Fecha final </strong></small> </th>
+     <th scope="col"><small><strong>Fecha de pago</strong></small> </th>
+     <th scope="col"><small><strong>Pago</strong></small> </th>
+     <th scope="col"><small><strong>Cantidad </strong></small> </th>
+     <th scope="col"><small><strong>Liberar</strong>  </small> </th>
+   </tr>
+ </thead>
+ <tbody class="  ">
 
+   <tr class="  ">
+     <td >Mensual</td>
+     <th scope="row">1</th>
+     <td>Otto</td>
+     <td>@mdo</td>
+    <td>@fat</td>
+     <td> <button class="btn-sm btn-warning" >Pagado</button> </td>
+   </tr>
+   <tr class="   ">
+     <td>Mensual</td>
+     <th scope="row">1</th>
+     <td>Otto</td>
+     <td>@mdo</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-warning" >Pagado</button></td>
+   </tr>
+   <tr class="   bg-white rounded">
+     <td>Mensual</td>
+     <th scope="row">1</th>
+     <td>Otto</td>
+     <td>@mdo</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-success">Cobrar</button></td>
+   </tr>
+   <tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr>
+   <tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr><tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr><tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr><tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr><tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr><tr class="   bg-white rounded">
+     <td class="text-info">Club</td>
+     <th scope="row">3</th>
+     <td>the Bird</td>
+     <td>@twitter</td>
+      <td>@fat</td>
+     <td> <button class="btn-sm btn-info" >Pendiente</button></td>
+   </tr>
+ </tbody>
+</table>
+</div>
+      </div>
+  <div class="col-sm-1  "><!-- IGNORE --></div>
+
+      <div class="col-sm-3 shadow bg-white rounded">
+        <div class="container">
+    <div class="row">
+      <div class="col-sm">
+
+      </div>
+      <div class="col-sm">
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
+
+      <div class="card-body text-primary">
+        <h3 class="text-center">1</h3>
+        <p class="text-center">Contrato <br><small>Mensual</small> </p>
+      </div>
+</div>
+      </div>
+    </div>
+  </div>
+      </div>
+    </div>
+  </div>
 
 
 </app-root>
